7e27fa491903b452c1cb43f7309263859a6136d4
Mr.Ak <akhilsaivaraganti@gmail.com>
2021-01-21 14:09:18 +0530

    need to work on else block

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 
+
+function add(){
+
+    let tasks = document.querySelector("#tasks");
+    let inputTask = document.querySelector("#inputTask");
+    let checkbox = document.createElement("input");
+    let label = document.createElement("label");
+    let lineBreak = document.createElement("br");
+    checkbox.type = "checkbox";
+    label.innerHTML = inputTask.value;
+    
+    if (inputTask != ""){ 
+        tasks.appendChild(checkbox);
+        tasks.appendChild(label);
+        tasks.appendChild(lineBreak);
+        inputTask.value = "";
+        console.log(inputTask.value)
+    }
+    else{
+        alert("enter the task");
+    }
+}
