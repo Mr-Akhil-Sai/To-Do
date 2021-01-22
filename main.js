5c890c2123dbecaef2a9b475a34ac717fb5fde71
Mr.AK <akhilsaivaraganti@gmail.com>
2021-01-22 09:01:02 +0530

    need to work on delete function

--- main.js
+++ main.js
@@ -4,19 +4,27 @@ function add(){
     let inputTask = document.querySelector("#inputTask");
     let checkbox = document.createElement("input");
     let label = document.createElement("label");
+    let crossButton = document.createElement("button");
     let lineBreak = document.createElement("br");
 
+
+    crossButton.innerHTML = "<i class='lni lni-cross-circle'></i>";
+    crossButton.onclick = del(label);
     checkbox.type = "checkbox";
     label.innerHTML = inputTask.value;
     
     if (inputTask.value){ 
         tasks.appendChild(checkbox);
         tasks.appendChild(label);
+        tasks.appendChild(crossButton);
         tasks.appendChild(lineBreak);
         inputTask.value = "";
-        console.log(inputTask.value)
     }
     else{
         alert("enter the task");
     }
+}
+ 
+function del(){
+    
 }
