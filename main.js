1db9c4794ed11b1e0785259177b6d3947e4d30be
Mr.AK <akhilsaivaraganti@gmail.com>
2021-01-22 13:00:26 +0530

    implemented delete functionality

--- main.js
+++ main.js
@@ -1,30 +1,52 @@
-function add(){
 
+
+
+function styles(crossButton, checkbox, label){
+    crossButton.style.position = "absolute";
+    crossButton.style.right = "10%";
+    crossButton.style.border = "none";
+    crossButton.style.marginTop = ".1em";
+    checkbox.style.marginRight = ".5em";
+    checkbox.style.fontSize = "2rem";
+    label.style.fontSize = "1.5rem";
+}
+
+
+function add(){    
     let tasks = document.querySelector("#tasks");
     let inputTask = document.querySelector("#inputTask");
     let checkbox = document.createElement("input");
     let label = document.createElement("label");
     let crossButton = document.createElement("button");
     let lineBreak = document.createElement("br");
-
-
+    
+    
     crossButton.innerHTML = "<i class='lni lni-cross-circle'></i>";
-    crossButton.onclick = del(label);
     checkbox.type = "checkbox";
     label.innerHTML = inputTask.value;
-    
+
+   
     if (inputTask.value){ 
+        
         tasks.appendChild(checkbox);
         tasks.appendChild(label);
         tasks.appendChild(crossButton);
+        styles(crossButton, checkbox, label);
         tasks.appendChild(lineBreak);
         inputTask.value = "";
+        if(checkbox.checked){
+            label.style.textDecoration= "line-through";
+            console.log("hi")
+        }
+        
+        crossButton.onclick = function(){ 
+           tasks.removeChild(checkbox);
+           tasks.removeChild(label);
+           tasks.removeChild(crossButton);
+        }
     }
+    
     else{
         alert("enter the task");
     }
-}
- 
-function del(){
-    
 }
